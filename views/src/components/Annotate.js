59c29a099f7e583baa93e45b6d8dc3f90ed02ffa
Hugh White <hdwhite@Hughs-MacBook-Pro.local>
2019-10-15 17:24:32 -0400

    put all files in views folder for realsies

---
+++ views/src/components/Annotate.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+import Container from "react-bootstrap/Container"
+import InputGroup from "react-bootstrap/InputGroup"
+import FormControl from "react-bootstrap/FormControl"
+import Button from "react-bootstrap/Button"
+
+class Home extends Component {
+  render() {
+    return (
+      <Container>
+        
+      </Container>
+    )
+  }
+}
+
+export default Home
