84a7b80f722368ff70106b8c11a626e900fb2813
Yu-Ji19 <ji.yu.vy@gmail.com>
2019-10-15 19:02:09 -0400

    setting up walking skeleton routers

---
+++ views/build/precache-manifest.b761b3be949585c6ffa88e58dff5c48e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92616dc833276bcebfd54b268325135d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d52b8b6446efb9c60fb",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6fff60f94a9b5554aab5",
+    "url": "/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "5d52b8b6446efb9c60fb",
+    "url": "/static/js/2.c755018f.chunk.js"
+  },
+  {
+    "revision": "6fff60f94a9b5554aab5",
+    "url": "/static/js/main.636de437.chunk.js"
+  },
+  {
+    "revision": "050ea787ce4f01b67dc8",
+    "url": "/static/js/runtime-main.5ca932a0.js"
+  }
+]);
