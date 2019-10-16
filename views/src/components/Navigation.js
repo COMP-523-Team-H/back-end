5e9c13015654cc583b26347c96970fb2996fd68e
Hugh White <hughdwhite@gmail.com>
2019-10-15 23:50:04 -0400

    testing reload problem

--- views/src/components/Navigation.js
+++ views/src/components/Navigation.js
@@ -6,11 +6,11 @@ class Navigation extends Component {
   render() {
     return (
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Article Annotator</Navbar.Brand>
+        <Navbar.Brand href="/">Article Annotator</Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
